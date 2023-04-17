8893a46eede8b3d54fb7ad582593f95cce991a98
PPnaruto <prtaik.35.patil@gmail.com>
2023-04-17 13:57:32 +0530

    Basic CSS and HTML has done with default image and HTML content added

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const h1 = document.createElement("h1");
-
-const ans = document.getElementById("handler_box");
-h1.innerText = "Custom Umbrella";
-ans.append(h1);
+// const h1 = document.createElement("h1");
+// const ans = document.getElementById("handler_box");
+// h1.innerText = "Custom Umbrella";
+// ans.append(h1);
